e1bbef560642e427b5f2eb732954ecee7f905488
Philip Hazelden <philip.hazelden@gmail.com>
2015-09-29 21:46:22 +0100

    Add a dropdown to select country manually.

--- assets/js/shoppr.js
+++ assets/js/shoppr.js
@@ -1,20 +1,40 @@
 $(function () {
     var countries = {
-        GB: { url: '...',
-              tag: '...',
+        GB: { url: 'UK URL',
+              tag: 'UK TAG',
               name: "United Kingdom" },
         CA: { url: "http://www.amazon.ca/exec/obidos/external-search",
               tag: "effectivecharities-21",
               name: "Canada" },
-        US: { url: '...',
-              tag: '...',
+        US: { url: 'USA URL',
+              tag: 'USA TAG',
               name: "United States" },
     }
 
+    var dropdown = $('#amazon-country');
+    for (var c in countries) {
+        var country = countries[c];
+        $('<option>').val(c).text(country.name).appendTo(dropdown);
+    }
+    dropdown.show();
+
+    var manuallySelected = false;
+    dropdown.on('change', function () {
+        setCountry($(this).val());
+        manuallySelected = true;
+    });
+
     $.get("http://ipinfo.io", function (response) {
-        var country = countries[response.country];
-        $('#amazon-form').attr('action', country.url);
-        $('#amzon-tag').val(country.tag);
-        console.log(response);
+        if (manuallySelected)
+            return;
+
+        setCountry(response.country);
+        $('#amazon-country').val(response.country);
     }, "jsonp");
+
+    function setCountry(code) {
+        var country = countries[code];
+        $('#amazon-form').attr('action', country.url);
+        $('#amazon-tag').val(country.tag);
+    }
 })
